b28106471addb30036c338ea3f1746a4509fd839
rbarbazz <raphael.barbazza@gmail.com>
2020-05-25 23:29:48 -0700

    feat(app): initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app id="app">
+    <v-content>
+      <v-container fluid>
+        <v-row>
+          <v-col
+            align="center"
+            class="ml-auto"
+            cols="12"
+            justify="start"
+            style="max-width: 300px; margin: auto;"
+          >
+            <TreeLogo style="margin-bottom: 20px;" />
+            <v-text-field
+              @input="onChange"
+              append-icon="mdi-magnify"
+              color="#41b913"
+              label="Solo"
+              placeholder="Search for a plant..."
+              solo
+            >
+            </v-text-field>
+            <v-btn @click="onClick" color="#41b913" style="color: white;"
+              >Search</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import TreeLogo from './components/TreeLogo.vue';
+
+const App = Vue.extend({
+  components: { TreeLogo },
+  data: () => ({ searchInput: '' }),
+  methods: {
+    onClick() {
+      console.log(this.searchInput);
+    },
+    onChange(value: string) {
+      this.searchInput = value;
+    },
+  },
+});
+
+export default App;
+</script>
---
+++ src/components/TreeLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    width="180"
+    height="180"
+    xmlns="http://www.w3.org/2000/svg"
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    fill="#41b913"
+  >
+    <path
+      d="M18.918 10.003h-1.828l3.318 5h-2.684l3.316 5h-8.018l.001 4h-2l-.001-4h-8.022l3.332-5h-2.672l3.295-5h-1.838l6.907-10.003 6.894 10.003zm-9.99-2h1.739l-3.294 5h2.694l-3.331 5h10.578l-3.315-5h2.682l-3.319-5h1.749l-3.089-4.481-3.094 4.481z"
+    /></svg
+></template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+const TreeLogo = Vue.extend({});
+
+export default TreeLogo;
+</script>
